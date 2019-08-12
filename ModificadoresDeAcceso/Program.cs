4b63de9cb4f3a5d68988165b1f261e2913551ffb
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-12 12:45:09 -0700

    Agregar archivos de proyecto.

---
+++ ModificadoresDeAcceso/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadoresDeAcceso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+
+            cliente.Nombre = "Carlos Encinas";
+            cliente.Telefono = "6444205599";
+
+
+            Console.WriteLine("Nombre:      " + cliente.Nombre);
+            Console.WriteLine("Teléfono:    " + cliente.Telefono);
+
+            try
+            {
+                cliente.Curp = "12345678912345678";
+                Console.WriteLine("CURP:        " + cliente.Curp);
+
+            } catch(Exception e)
+            {
+                Console.WriteLine("CURP:        " + "La CURP debe ser de 18 caracteres");
+            }
+            
+
+            Console.ReadLine();
+            
+        }
+    }
+}
