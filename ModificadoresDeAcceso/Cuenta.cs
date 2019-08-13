469b9e69f5b06874aeb7cc5318d52d5ef6e5c09e
Carlos Encinas <carlos-encinas@hotmail.com>
2019-08-13 07:49:38 -0700

    Clase Cuenta

---
+++ ModificadoresDeAcceso/Cuenta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadoresDeAcceso
+{
+    class Cuenta
+    {
+        public Cliente Propietario { get; set; }
+        private string _identificador { get; set; }
+        public string Identificador
+        {
+            get
+            {
+                return _identificador;
+            }
+            set
+            {
+                if (value.Length < 10 && value.Length <= 14)
+                    _identificador = value;
+                else
+                    throw new Exception("Su ID debe tener 10 - 14 caracteres");
+            }
+        }
+        public float Saldo { get; set; }
+
+        public void Retirar()
+        {
+
+        }
+
+        public void Depositar()
+        {
+
+        }
+
+
+    }
+}
--- ModificadoresDeAcceso/Program.cs
+++ ModificadoresDeAcceso/Program.cs
@@ -31,7 +31,7 @@ namespace ModificadoresDeAcceso
             
 
             Console.ReadLine();
-            
+        
         }
     }
 }
